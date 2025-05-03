3672320bfb69c1ca6629540e7e629c2fb06f3fe9
Sage Liem <sage.liem.9@gmail.com>
2025-05-03 15:34:26 -0500

    Add buffer labels, fix flickering updates

--- tui.cpp
+++ tui.cpp
@@ -6,57 +6,111 @@
 TuiApp::TuiApp()
   : width{},
     height{}
-{}
+{
+    for (int i=0; i<8; ++i) {
+        windows[i] = std::make_unique<BufferWindow>(i);
+    }
+}
 
 
 void TuiApp::setup() 
 {
     initscr();
-    
     getmaxyx(stdscr, height, width);
-    box(stdscr, 0, 0);
-    wrefresh(stdscr);
+    for (int i=0; i<8; ++i) {
+        windows[i]->setup(nullptr, height/8, width, height/8*i, 0);
+    }
+}
+
+void TuiApp::load_buffer( const int id, Player *p )
+{
+    windows[id]->load_player(p);
 }
 
 void TuiApp::update()
 {
-    werase(stdscr);
-    box(stdscr, 0, 0);
+//    werase(stdscr);
+    //box(stdscr, 0, 0);
     
-
+    for (std::unique_ptr<BufferWindow> &window : windows)
+    {
+        window->update();
+    }
 
 
     
-    wrefresh(stdscr);
+    doupdate();
 }
 
 void TuiApp::close()
 {
+    for (std::unique_ptr<BufferWindow> &window : windows)
+    {
+        window->close();
+    }
     delwin(stdscr);
     endwin();
 }
 
 
+
 // Window class for each buffer
 BufferWindow::BufferWindow(const char id_init)
   : p { nullptr },
+    win { nullptr },
     id { id_init },
     name { "" },
     active { false }
 {}
 
-void BufferWindow::setup(Player *p_init)
+// Create window
+void BufferWindow::setup(Player *p_init, const int h_init, const int w_init, const int y_init, const int x_init)
 {
+    height = h_init;
+    width = w_init;
+
+    y = y_init;
+    x = x_init;
+
+    // Initialize window element
+    win = newwin(height, width, y, x);
+
     p = p_init;
-    name = p->getPath();
+    if (p) {
+        name = p->getPath();
+    }
 }
 
+void BufferWindow::load_player(Player *p_new)
+{
+    p = p_new;
+    if (p)
+    {
+        name = p->getPath();
+    }
+}
+
+// Update window each frame
 void BufferWindow::update()
 {
+    werase(win);
 
+    box(win, 0, 0);
+
+    mvwprintw(win, 0, 2, "| %d | ", id);
+    wprintw(win, name.c_str());
+
+    if (p)
+    {
+        double timepos = p->getCurrentPos();
+        wprintw(win, " | %.2f |", timepos);
+    }
+
+    wnoutrefresh(win);
 }
 
+// Close a buffer window element
 void BufferWindow::close()
 {
-
+    delwin(win);
 }
--- tui.hpp
+++ tui.hpp
@@ -3,27 +3,38 @@
 #include <ncurses.h>
 #include "player.hpp"
 #include <string>
+#include <array>
+
+class BufferWindow
+{
+    WINDOW *win;
+    Player *p; 
+    const char id;
+    std::string name;
+    bool active;
+    int height;
+    int width;
+    int x;
+    int y;
+public:
+    BufferWindow(const char id);
+    void setup(Player *p_init, const int h_init, const int w_init, const int y_init, const int x_init);
+    void load_player(Player *p_new);
+    void update();
+    void close();
+};
 
 class TuiApp 
 {
     int width;
     int height;
+    std::array<std::unique_ptr<BufferWindow>, 8> windows;
 public:
     TuiApp();
     void setup();
     void update();
     void close();
+    void load_buffer(const int id, Player *p);
 };
 
-class BufferWindow
-{
-    Player *p; 
-    const char id;
-    std::string name;
-    bool active;
-public:
-    BufferWindow(const char id);
-    void setup(Player *p_init);
-    void update();
-    void close();
-};
+
