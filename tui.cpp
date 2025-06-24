8e6077b26d514d36aecbf9cdd834c2473e9b9a66
Sage Liem <sliem@fieldmuseum.org>
2025-06-24 16:07:30 -0500

    Remove direct access to Players

--- tui.cpp
+++ tui.cpp
@@ -5,7 +5,7 @@
 // Create TUI app class
 TuiApp::TuiApp() : width{}, height{} {
   for (int i = 0; i < 8; ++i) {
-    windows[i] = std::make_unique<BufferWindow>(i);
+    windows[i] = std::make_unique<PlayerWindow>(i);
   }
 }
 
@@ -14,21 +14,20 @@ void TuiApp::setup() {
   initscr();
   getmaxyx(stdscr, height, width);
   for (int i = 0; i < 8; ++i) {
-    //        windows[i]->setup(nullptr, height/8, width, height/8*i, 0);
+    windows[i]->setPos(height / 8, width, height / 8 * i, 0);
   }
 }
 
-// Load buffer window object at index id with Player
-void TuiApp::load_buffer(const int id, std::unique_ptr<Player> &p) {
-  windows[id]->load_player(p);
-}
-
 // TODO
-void TuiApp::setActiveBuffer(const int id) {}
+void TuiApp::setActiveWindow(const int &id) {}
+
+const std::unique_ptr<PlayerWindow> &TuiApp::getWindow(const int &id) {
+  return windows[id];
+}
 
 // Called each frame, updates each window and then displays to screen
 void TuiApp::update() {
-  for (std::unique_ptr<BufferWindow> &window : windows) {
+  for (std::unique_ptr<PlayerWindow> &window : windows) {
     window->update();
   }
 
@@ -37,7 +36,7 @@ void TuiApp::update() {
 
 // Closes windows, main screen, and exits ncurses mode
 void TuiApp::close() {
-  for (std::unique_ptr<BufferWindow> &window : windows) {
+  for (std::unique_ptr<PlayerWindow> &window : windows) {
     window->close();
   }
   delwin(stdscr);
@@ -49,8 +48,8 @@ void TuiApp::close() {
 //     : win{nullptr}, p{nullptr}, id{id_init}, name{""} {}
 //
 // Create window displaying player info
-void BufferWindow::setup(std::unique_ptr<Player> &p_init, const int h_init,
-                         const int w_init, const int y_init, const int x_init) {
+void PlayerWindow::setPos(const int h_init, const int w_init, const int y_init,
+                          const int x_init) {
   height = h_init;
   width = w_init;
 
@@ -59,8 +58,6 @@ void BufferWindow::setup(std::unique_ptr<Player> &p_init, const int h_init,
 
   // Initialize window element
   win = newwin(height, width, y, x);
-
-  load_player(p_init);
 }
 
 // Redirect buffer window to a different player
@@ -72,7 +69,7 @@ void BufferWindow::setup(std::unique_ptr<Player> &p_init, const int h_init,
 // }
 
 // Update window each frame, displaying player info if available
-void BufferWindow::update() {
+void PlayerWindow::update() {
   werase(win);
 
   box(win, 0, 0);
@@ -80,22 +77,18 @@ void BufferWindow::update() {
   mvwprintw(win, 0, 2, "| %d | ", id);
 
   // Display player info if player is loaded
-  if (p) {
-    wprintw(win, "%s", name.c_str());
+  wprintw(win, "%s", name.c_str());
 
-    double timepos = p->getCurrentPos();
-    wprintw(win, " | %.2f |", timepos);
+  wprintw(win, " | %.2f |", timePos);
 
-    mvwprintw(win, 2, 2, "loop[%.2f, %.2f]", p->getLoopStart(),
-              p->getLoopEnd());
-    mvwprintw(win, 3, 2, "speed[%.2f]", p->getSpeed());
-    mvwprintw(win, 4, 2, "pitch[%.2f]", p->getPitch());
-    mvwprintw(win, 5, 2, "volume[%.2f]", p->getVolume());
-  }
+  mvwprintw(win, 2, 2, "loop[%.2f, %.2f]", loopStart, loopEnd);
+  mvwprintw(win, 3, 2, "speed[%.2f]", speed);
+  mvwprintw(win, 4, 2, "pitch[%.2f]", pitch);
+  mvwprintw(win, 5, 2, "volume[%.2f]", volume);
 
   // Print contents to virtual screen
   wnoutrefresh(win);
 }
 
 // Close buffer window element
-void BufferWindow::close() { delwin(win); }
+void PlayerWindow::close() { delwin(win); }
--- tui.hpp
+++ tui.hpp
@@ -1,26 +1,57 @@
 #pragma once
 
-#include "player.hpp"
-#include <array>
 #include <memory>
 #include <ncurses.h>
 #include <string>
+#include <vector>
 
-class BufferWindow {
+// class BufferWindow {
+//   WINDOW *win;
+//   std::unique_ptr<Player> &p;
+//   const char id;
+//   std::string name;
+//   int height;
+//   int width;
+//   int x;
+//   int y;
+//
+// public:
+//   BufferWindow(const char id);
+//   void setup(std::unique_ptr<Player> &p_init, const int h_init,
+//              const int w_init, const int y_init, const int x_init);
+//   void load_player(std::unique_ptr<Player> &p_new);
+//   void update();
+//   void close();
+// };
+//
+
+class PlayerWindow {
   WINDOW *win;
-  std::unique_ptr<Player> &p;
-  const char id;
+  const int id;
   std::string name;
   int height;
   int width;
   int x;
   int y;
+  double timePos;
+  double loopStart;
+  double loopEnd;
+  double speed;
+  double pitch;
+  double volume;
 
 public:
-  BufferWindow(const char id);
-  void setup(std::unique_ptr<Player> &p_init, const int h_init,
-             const int w_init, const int y_init, const int x_init);
-  void load_player(std::unique_ptr<Player> &p_new);
+  PlayerWindow(const char id);
+  void setPos(const int h_init, const int w_init, const int y_init,
+              const int x_init);
+
+  void setName(std::string name);
+  void setTimePos(double pos);
+  void setLoop(double start, double end);
+  void setSpeed(double pos);
+  void setPitch(double pitch);
+  void setVolume(float pos);
+
   void update();
   void close();
 };
@@ -28,13 +59,13 @@ public:
 class TuiApp {
   int width;
   int height;
-  std::array<std::unique_ptr<BufferWindow>, 8> windows;
+  std::vector<std::unique_ptr<PlayerWindow>> windows;
 
 public:
   TuiApp();
   void setup();
   void update();
   void close();
-  void load_buffer(const int id, std::unique_ptr<Player> &p);
-  void setActiveBuffer(const int id);
+  void setActiveWindow(const int &id);
+  const std::unique_ptr<PlayerWindow> &getWindow(const int &id);
 };
