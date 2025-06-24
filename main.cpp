34da1105cb5a1013200745bd477270aeb56850ec
Sage Liem <sliem@fieldmuseum.org>
2025-06-24 00:32:15 -0500

    Use smart pointers for players, remove empty objects, disable TUI temporarily

--- main.cpp
+++ main.cpp
@@ -23,21 +23,25 @@ int main() {
   // Testing for OscHandler
   OscHandler osc(5050);
 
-  std::array<Player, 8> players{};
+  std::vector<std::unique_ptr<Player>> players{};
 
-  Controller controller(&players);
+  Controller controller(players);
 
-  bool showUi = false;
+  // bool showUi = false;
 
   //    jack_client_t* jack = jack_client_open("ytcc", JackNullOption, nullptr);
   //    jack_set_process_callback(jack, )
 
   // Set up MIDI control
-  // MidiHandler midi;
-  // midi.setup( &controller );
+  MidiHandler midi;
+  midi.setup(&controller);
+  midi.setRelativeKnobs(true);
+
+  players.push_back(std::make_unique<Player>());
+  players.push_back(std::make_unique<Player>());
 
   // Initialize players
-  //    players[0].load("https://www.youtube.com/watch?v=-ASxhgeVBpo"); // ECM
+  players[0]->load("https://www.youtube.com/watch?v=-ASxhgeVBpo"); // ECM
   //    trumpet
   //    players[1].load("https://youtu.be/4CaSXEEqKBU?si=HU0oqygxxOOSojvL");
   //    p.load("https://www.youtube.com/watch?v=DkLEZ0-l70U&list=LL&index=59");
@@ -45,9 +49,8 @@ int main() {
   //    p.load("https://www.youtube.com/watch?v=5Dkf6UUfwQk&list=PLuoCvADUeA_KmRoDuAu4zWRksB8lnpkKC&index=20");
   //    // Neanic p.load("https://www.youtube.com/watch?v=CxPSbe8xNyQ"); //
   //    Grant Gordy players[0].load("./handpan.wav");
-
-  players[0].load("https://www.youtube.com/watch?v=oxQmec6gGV0");
-  players[1].load("https://www.youtube.com/shorts/HzfMYiGj7M4");
+  //    players[0]->load("https://www.youtube.com/watch?v=oxQmec6gGV0");
+  //  players[1]->load("https://www.youtube.com/shorts/HzfMYiGj7M4");
   // players[2].load("https://www.youtube.com/watch?v=FY1CwzIjoJQ");
   //    players[3].load("https://www.youtube.com/watch?v=ngfBSBoYzUY");
 
@@ -55,34 +58,34 @@ int main() {
   std::cout << "Initialized OSC listener\n";
 
   // Set up TUI
-  TuiApp ui;
-
-  if (showUi) {
-    ui.setup();
-    ui.load_buffer(0, &(players[0]));
-    ui.load_buffer(1, &(players[1]));
-
-    ui.load_buffer(2, &(players[2]));
-    ui.load_buffer(3, &(players[3]));
-  } else {
-    std::cout << "UI Disabled\n";
-  }
-
+  // TuiApp ui;
+  //
+  // if (showUi) {
+  //   ui.setup();
+  //   ui.load_buffer(0, &(players[0]));
+  //   ui.load_buffer(1, &(players[1]));
+  //
+  //   ui.load_buffer(2, &(players[2]));
+  //   ui.load_buffer(3, &(players[3]));
+  // } else {
+  //   std::cout << "UI Disabled\n";
+  // }
+  //
   // Main loop
   while (1) {
 
     controller.update();
-    for (Player &p : players) {
+    for (std::unique_ptr<Player> &p : players) {
       try {
-        p.update();
+        p->update();
       } catch (std::exception &err) {
         std::cerr << "Exception while updating players: " << err.what() << '\n';
-        if (showUi)
-          ui.close();
-        return 1;
+        // if (showUi)
+        //   ui.close();
+        // return 1;
       } catch (...) {
-        if (showUi)
-          ui.close();
+        // if (showUi)
+        // ui.close();
         return 1;
       }
     }
@@ -92,12 +95,12 @@ int main() {
     //        std::cout << "Current time: " << p.getCurrentPos() << " Loop: ["
     //        << p.getLoopStart() << ", " << p.getLoopEnd() << "]\n";
 
-    if (showUi)
-      ui.update();
+    // if (showUi)
+    // ui.update();
   }
 
-  if (showUi)
-    ui.close();
+  // if (showUi)
+  // ui.close();
 
   return 0;
 }
