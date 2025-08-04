ad557700d93cc2be579380c805631354260d6f61
Sage Liem <sage.liem.9@gmail.com>
2025-08-04 15:50:59 -0500

    Remove playerWindow class

--- tui.cpp
+++ tui.cpp
@@ -3,11 +3,8 @@
 #include <iostream>
 
 // Create TUI app class
-TuiApp::TuiApp() {
-  for (int i = 0; i < 8; i++) {
-    windows.push_back(std::make_unique<PlayerWindow>(i));
-  }
-}
+TuiApp::TuiApp(std::vector<std::unique_ptr<Player>> &players)
+    : players{players} {}
 
 // setup(): enter ncurses mode and create windows
 void TuiApp::setup() {
@@ -17,92 +14,39 @@ void TuiApp::setup() {
     return;
   }
   getmaxyx(stdscr, height, width);
-  for (int i = 0; i < 8; i++) {
-    windows[i]->setPos(0, (height / 8) * i, width, height / 8);
-  }
 }
 
 // TODO
-void TuiApp::setActiveWindow(const int &id) {
-  for (const auto &window : windows)
-    window->setActive(false);
-  windows[id]->setActive(true);
-}
-
-std::unique_ptr<PlayerWindow> &TuiApp::getWindow(const int &id) {
-  return windows[id];
-}
+void TuiApp::setActiveWindow(const int &id) {}
 
 // Called each frame, updates each window and then displays to screen
 void TuiApp::update() {
-  for (std::unique_ptr<PlayerWindow> &window : windows) {
-    window->update();
+  int counter = 1;
+  for (std::unique_ptr<Player> &player : players) {
+    mvwprintw(stdscr, 0, 2, "| %d | ", counter);
+    // Display player info if player is loaded
+    wprintw(stdscr, "%s", player->getTitle().c_str());
+
+    wprintw(stdscr, " | %.2f |", player->getCurrentPos());
+
+    mvwprintw(stdscr, 2, 2, "loop[%.2f, %.2f]", player->getLoopStart(),
+              player->getLoopEnd());
+    mvwprintw(stdscr, 3, 2, "speed[%.2f]", player->getSpeed());
+    mvwprintw(stdscr, 4, 2, "pitch[%.2f]", player->getPitch());
+    mvwprintw(stdscr, 5, 2, "volume[%.2f]", player->getVolume());
+
+    // Print contents to virtual screen
+    wnoutrefresh(stdscr);
   }
+  // for (std::unique_ptr<PlayerWindow> &window : windows) {
+  //   window->update();
+  // }
 
   doupdate(); // Display to screen
 }
 
 // Closes windows, main screen, and exits ncurses mode
 void TuiApp::close() {
-  for (std::unique_ptr<PlayerWindow> &window : windows) {
-    window->close();
-  }
   delwin(stdscr);
   endwin();
 }
-
-//
-// PlayerWindow class
-//
-PlayerWindow::PlayerWindow(const int id) : id{id} {}
-
-void PlayerWindow::setPos(const int x, const int y, const int w, const int h) {
-  height = h;
-  width = w;
-
-  this->y = y;
-  this->x = x;
-
-  // Initialize window element
-  win = newwin(height, width, y, x);
-}
-
-// Update window each frame, displaying player info if available
-void PlayerWindow::update() {
-  werase(win);
-
-  if (active)
-    box(win, '0', '0');
-  else
-    box(win, 0, 0);
-
-  mvwprintw(win, 0, 2, "| %d | ", id);
-
-  // Display player info if player is loaded
-  wprintw(win, "%s", name.c_str());
-
-  wprintw(win, " | %.2f |", timePos);
-
-  mvwprintw(win, 2, 2, "loop[%.2f, %.2f]", loopStart, loopEnd);
-  mvwprintw(win, 3, 2, "speed[%.2f]", speed);
-  mvwprintw(win, 4, 2, "pitch[%.2f]", pitch);
-  mvwprintw(win, 5, 2, "volume[%.2f]", volume);
-
-  // Print contents to virtual screen
-  wnoutrefresh(win);
-}
-
-void PlayerWindow::close() { delwin(win); }
-
-void PlayerWindow::setName(const std::string &name) { this->name = name; }
-void PlayerWindow::setTimePos(const double &pos) { this->timePos = pos; }
-void PlayerWindow::setLoopStart(const double &start) {
-  this->loopStart = start;
-}
-void PlayerWindow::setLoopLength(const double &loopLength) {
-  loopEnd = loopEnd + loopLength;
-}
-void PlayerWindow::setSpeed(const double &speed) { this->speed = speed; }
-void PlayerWindow::setPitch(const double &pitch) { this->pitch = pitch; }
-void PlayerWindow::setVolume(const double &volume) { this->volume = volume; }
-void PlayerWindow::setActive(bool opt) { this->active = opt; }
--- tui.hpp
+++ tui.hpp
@@ -2,54 +2,19 @@
 
 #include <memory>
 #include <ncurses.h>
-#include <string>
 #include <vector>
 
-class PlayerWindow {
-  WINDOW *win;
-  const int id;
-  std::string name;
-  bool active;
-  int height;
-  int width;
-  int x;
-  int y;
-  double timePos;
-  double loopStart;
-  // double loopLength;
-  double loopEnd;
-  double speed;
-  double pitch;
-  double volume;
-
-public:
-  PlayerWindow(const int id);
-  void setPos(const int h_init, const int w_init, const int y_init,
-              const int x_init);
-
-  void setName(const std::string &name);
-  void setTimePos(const double &pos);
-  void setLoopStart(const double &start);
-  void setLoopLength(const double &loopLength);
-  void setSpeed(const double &pos);
-  void setPitch(const double &pitch);
-  void setVolume(const double &volume);
-  void setActive(bool opt);
-
-  void update();
-  void close();
-};
+#include "player.hpp"
 
 class TuiApp {
   int width;
   int height;
-  std::vector<std::unique_ptr<PlayerWindow>> windows;
+  std::vector<std::unique_ptr<Player>> &players;
 
 public:
-  TuiApp();
+  TuiApp(std::vector<std::unique_ptr<Player>> &players);
   void setup();
   void update();
   void close();
   void setActiveWindow(const int &id);
-  std::unique_ptr<PlayerWindow> &getWindow(const int &id);
 };
